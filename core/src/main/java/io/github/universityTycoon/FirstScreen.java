1342deac6b65e4969a41fd1cc8eb40213d97c974
AdamYork2211 <ak2211@york.ac.uk>
2025-01-12 18:31:36 +0000

    errorhandling and added methods

--- core/src/main/java/io/github/universityTycoon/FirstScreen.java
+++ core/src/main/java/io/github/universityTycoon/FirstScreen.java
@@ -11,6 +11,7 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Disposable;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
